c77ebabd34e64a8c44d63c2f28427f3718ddbbda
Takeshi Onodera <t.onodera.1.2.1.5@gmail.com>
2021-07-24 00:47:40 +0900

    テンプレートでの表示を確認

--- quick_django/main/urls.py
+++ quick_django/main/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name="index")
+    path('', views.index, name="index"),
+    path('temp', views.temp, name="temp")
 ]
--- quick_django/main/views.py
+++ quick_django/main/views.py
@@ -4,3 +4,10 @@ from django.http import HttpRequest, HttpResponse
 
 def index(request: HttpRequest):
     return HttpResponse('こんにちは、世界！')
+
+
+def temp(request: HttpRequest):
+    context = {
+        'msg': 'こんにちは、世界！'
+    }
+    return render(request, 'main/temp.html', context)
