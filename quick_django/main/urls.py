e2f85efac9528b8c629f8f16c1433236061f1802
Takeshi Onodera <t.onodera.1.2.1.5@gmail.com>
2021-07-24 07:43:04 +0900

    part2完了

--- quick_django/main/urls.py
+++ quick_django/main/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
-    path('temp', views.temp, name="temp")
+    path('temp', views.temp, name="temp"),
+    path('list', views.list, name="list")
 ]
--- quick_django/main/views.py
+++ quick_django/main/views.py
@@ -1,3 +1,4 @@
+from .models import Book
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
 
@@ -11,3 +12,10 @@ def temp(request: HttpRequest):
         'msg': 'こんにちは、世界！'
     }
     return render(request, 'main/temp.html', context)
+
+
+def list(request: HttpRequest):
+    books = Book.objects.all()
+    return render(request, 'main/list.html', {
+        'books': books
+    })
