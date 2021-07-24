8a0190a2fbd9b3b6cf29172da5a94278ecb1f699
Takeshi Onodera <t.onodera.1.2.1.5@gmail.com>
2021-07-24 19:17:16 +0900

    4完了

--- quick_django/main/urls.py
+++ quick_django/main/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('temp', views.temp, name="temp"),
     path('list', views.list, name="list"),
     path('rel', views.rel, name="rel"),
+    path('rel2', views.rel2, name="rel2"),
 ]
