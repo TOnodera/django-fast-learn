d4b2618cff16079c338dc2e43d79ef4ba6b50d5d
Takeshi Onodera <t.onodera.1.2.1.5@gmail.com>
2021-07-24 18:34:16 +0900

    モデルにリレーション貼った

--- quick_django/main/models.py
+++ quick_django/main/models.py
@@ -37,3 +37,21 @@ class Book(models.Model):
     # モデルの文字列表現
     def __str__(self):
         return f'{self.title}({self.publisher}/{self.price})円'
+
+
+class Review(models.Model):
+
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+
+    name = models.CharField(
+        verbose_name='名前',
+        max_length=20
+    )
+
+    body = models.TextField(
+        verbose_name='本文',
+        max_length=255
+    )
+
+    def __str__(self) -> str:
+        return f'{self.name}: {self.body[:10]}'
